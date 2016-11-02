96d139ac7bd8e63e95fd8617b7ea8751e4991ab4
Paul Kearney <paul.kearney1@dwp.gsi.gov.uk>
2016-11-02 22:56:55 +0000

    Add gui profile and role using xfce4

---
+++ site/profile/manifests/gui.pp
@@ -0,0 +1,20 @@
+class profile::gui {
+
+  package { 'xfce4':
+    ensure => installed,
+  }
+  
+  package { 'gnome-icon-theme-full':
+    ensure => installed,
+  }
+  
+  package { 'tango-icon-theme':
+    ensure => installed,
+  }
+  
+  file_line { 'Edit /etc/X11/Xwrapper.config to allow anybody':
+    path => '/etc/X11/Xwrapper.config',  
+    line => 'allowed_users=anybody',
+    match   => "allowed_users=console",
+  }
+}
--- site/role/manifests/gui.pp
+++ site/role/manifests/gui.pp
@@ -1,3 +1,4 @@
 class role::gui {
    include profile::base
+   include profile::gui
 }

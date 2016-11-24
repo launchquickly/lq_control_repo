079325a67b4c536f73e32094d78f606b2d0ca018
Paul Kearney <paul.kearney1@dwp.gsi.gov.uk>
2016-11-24 20:48:39 +0000

    Fix Exec line in atom.desktop to prevent freezes

--- site/profile/manifests/atom.pp
+++ site/profile/manifests/atom.pp
@@ -1,8 +1,13 @@
 class profile::atom {
 
-  apt::ppa { 'ppa:webupd8team/atom': }
-  ->
+  apt::ppa { 'ppa:webupd8team/atom': 
+  }->
   package { 'atom':
     ensure   => 'present',
+  }->
+  file_line { 'Fix Exec on /usr/share/applications/atom.desktop':
+    path => '/usr/share/applications/atom.desktop',
+    line => 'Exec=env atom %F',
+    match => 'Exec=/opt/atom/atom %F',
   }
 }

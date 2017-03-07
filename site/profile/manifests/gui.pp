ec7b631af3d9cd1f855d85f81be021f0ff3b9d23
Paul Kearney <paulkearney@launchquickly.com>
2017-03-07 22:57:24 +0000

    Install xserver legacy is OS Ubuntu Xenial

--- site/profile/manifests/gui.pp
+++ site/profile/manifests/gui.pp
@@ -11,11 +11,21 @@ class profile::gui {
   package { 'tango-icon-theme':
     ensure => installed,
   }
+
+  file { '/etc/X11/Xwrapper.config':
+    ensure => present,
+  }
   
   file_line { 'Edit /etc/X11/Xwrapper.config to allow anybody':
     path => '/etc/X11/Xwrapper.config',  
     line => 'allowed_users=anybody',
     match   => "allowed_users=console",
   }
+
+  if $facts['os']['name'] == 'Ubuntu' and $facts['os']['release']['major'] == '16.04' {
+    package { 'xserver-xorg-legacy':
+      ensure => installed,
+    }
+  } 
   
 }

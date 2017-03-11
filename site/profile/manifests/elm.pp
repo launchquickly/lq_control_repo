3c5c0312054ff07471648a5d32f791f9e184f2d3
Paul Kearney <paulkearney@launchquickly.com>
2017-03-11 15:27:32 +0000

    Install elm-format binary when installing elm

--- site/profile/manifests/elm.pp
+++ site/profile/manifests/elm.pp
@@ -5,4 +5,13 @@ class profile::elm {
     provider => 'npm',
   }
 
+  archive { '/usr/local/bin/elm-format-0.18-0.5.2-alpha-linux-x64.tgz':
+    ensure        => present,
+    extract       => true,
+    extract_path  => '/usr/local/bin',
+    source        => 'https://github.com/avh4/elm-format/releases/download/0.5.2-alpha/elm-format-0.18-0.5.2-alpha-linux-x64.tgz',
+    creates       => '/usr/local/bin/elm-format',
+    cleanup       => true,
+  }
+
 }

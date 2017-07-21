412096895fdf575531536c0969dc16981476d212
Paul Kearney <paulkearney@launchquickly.com>
2017-07-21 22:51:20 +0000

    Remove install of Eclipse and Chrome

--- site/role/manifests/javadev.pp
+++ site/role/manifests/javadev.pp
@@ -1,10 +1,6 @@
 class role::javadev {
    include profile::base
-   include profile::gui
    include profile::java
    include profile::maven
-   include profile::eclipse
-   include profile::chrome
-   include profile::awscli
    include profile::utils
 }

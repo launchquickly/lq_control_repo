a6cf11ce7673f7b52f6e41a83eb5ee5c65676a1b
Paul Kearney <paulkearney@launchquickly.com>
2017-09-01 23:54:23 +0000

    Swith to visual studio code from Elm

--- site/role/manifests/elmdev.pp
+++ site/role/manifests/elmdev.pp
@@ -3,12 +3,9 @@ class role::elmdev {
    include profile::gui
    contain profile::nodejs
    contain profile::elm
-   contain profile::atom
-   contain profile::atomelm
+   include profile::visualstudiocode
    include profile::chrome
    include profile::utils
    
    Class['profile::nodejs'] -> Class['profile::elm']
-   Class['profile::nodejs'] -> Class['profile::atom']
-   Class['profile::atom'] -> Class['profile::atomelm']
 }

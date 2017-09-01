9d41a1904ee992bd340014b8f64940168bdea78b
Paul Kearney <paulkearney@launchquickly.com>
2017-09-01 23:45:40 +0000

    Add in java & nodejs into awsdev

--- site/role/manifests/awsdev.pp
+++ site/role/manifests/awsdev.pp
@@ -1,6 +1,9 @@
 class role::awsdev {
    include profile::base
    include profile::awscli
+   include profile::nodejs
+   include profile::java
+   include profile::maven
    include profile::visualstudiocode
    include profile::utils
 }

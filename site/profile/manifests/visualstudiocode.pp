666fe8c3b2ca43dbf609b775ed61b37713a0ecc3
Paul Kearney <paulkearney@launchquickly.com>
2017-09-01 23:17:25 +0000

    Fix syntax error in vs code install

--- site/profile/manifests/visualstudiocode.pp
+++ site/profile/manifests/visualstudiocode.pp
@@ -5,16 +5,16 @@ class profile::visualstudiocode {
     creates => '/tmp/microsoft.gpg',
   }->
   file { '/etc/apt/trusted.gpg.d/microsoft.gpg':
-    source => '/tmp/microsoft.gpg,
+    source => '/tmp/microsoft.gpg',
     owner   => 'root',
     group   => 'root',
-    mode    => 0644
+    mode    => '0644',
   }-> 
   file { '/etc/apt/sources.list.d/vscode.list':
     content => 'deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main',
     owner   => 'root',
     group   => 'root',
-    mode    => 0644
+    mode    => '0644',
   }->
   exec { 'apt-update':
     command => '/usr/bin/apt-get update',

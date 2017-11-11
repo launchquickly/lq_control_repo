51160edb6ce5ad8f62676388d1afb4032d1e1896
Paul Kearney <paulkearney@launchquickly.com>
2017-11-11 22:42:42 +0000

    Change how Hugo is installed

--- site/profile/manifests/hugo.pp
+++ site/profile/manifests/hugo.pp
@@ -1,5 +1,16 @@
 class profile::hugo {
 
-  include hugo
+  $version = '0.30.2'
 
+  wget::fetch { "https://github.com/gohugoio/hugo/releases/download/v${version}/hugo_${version}_Linux-64bit.deb":
+    destination => '/tmp/',
+    timeout     => 0,
+    verbose     => false,
+  }
+  
+  package { "hugo-${version}":
+    provider => dpkg,
+    ensure   => installed,
+    source   => "/tmp/hugo_${version}_Linux-64bit.deb",
+  }
 }
